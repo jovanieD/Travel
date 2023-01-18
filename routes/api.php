305f7df271fef265076791d769561ca443e3f94a
vanie <vanie>
2023-01-18 15:18:58 +0800

    controller

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\UserController;
-=======
 use App\Http\Controllers\AuthController;
->>>>>>> 832eb667ecc73fd5f0e708b808396489f61aeb1d
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,19 +19,15 @@ use App\Http\Controllers\AuthController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
 
 
-// Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('create', [UserController::class, 'store']);
     Route::delete('delete/{id}', [UserController::class, 'destroy']);
     Route::get('show/{id}', [UserController::class, 'show']);
     Route::put('update/{id}', [UserController::class, 'update']);
-// });
-=======
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
 });
+
+
+
 Route::post('/login', [AuthController::class,"login"]);
-Route::get('/show', [AuthController::class,"index"]);
->>>>>>> 832eb667ecc73fd5f0e708b808396489f61aeb1d
